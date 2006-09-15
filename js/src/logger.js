ae12fae6c2e6a3a2810226bd19a75b88e8f169b0
akm2000 <akm2000@907533e0-d619-0410-9558-a12620d830f9>
2006-09-15 10:43:54 +0000

    inplace_editorができた！・・・・と思う。 loggerが出力するログの量によって、時間をずらして処理するようにした。

--- js/src/logger.js
+++ js/src/logger.js
@@ -56,7 +56,9 @@ Object.extend(Logger, {
 	inspectCaption: function(obj){
 	   if (obj==undefined)return "undefined";
 	   if (obj==null)return "null";
-	   var idx = this.knownClasses.indexOf(obj.constructor);
+	   var objClass;
+	   try{objClass = obj.constructor;}catch(ex){objClass = "unknown class";}
+	   var idx = this.knownClasses.indexOf(objClass);
 	   var className = (idx>-1)?this.knownClassNames[idx]:(obj.nodeType)?this.identify_str(obj):null;
 	   var typeName=typeof(obj);
 	   return ("["+(className||typeName)+"]"+obj).truncate(100).gsub(/\n/,"\\n");
@@ -64,30 +66,57 @@ Object.extend(Logger, {
 	
 	likeArray: function(obj){
 	   if(!obj)return false;
-	   return (!obj.nodeType)&&(obj.constructor!=String)&&(obj.length!=null)&&(obj.length!=undefined)&&(obj.length.constructor==Number);
+	   var objClass;
+	   try{objClass = obj.constructor}catch(ex){objClass = "unknown class"};
+	   return (!obj.nodeType)&&
+	       (objClass!=String)&&
+	       (objClass!=Function)&& 
+	       (!obj.navigator)&&(!obj.frames)&& //except window
+	       (obj.length!=null)&&(obj.length!=undefined)&&
+	       (obj.length.constructor==Number);
 	},
 	
 	DefaultLevel:1,
 	DefaultMaxLevel:1,
 	
 	inspect_array: function(obj,predicate,level,max_level){
-       var s="[\n";
-       for(var i=0;i<obj.length;i++)
-           s+=(spacing+i+":"+Logger.inspect(obj[i],Logger.excludeFunction,level,max_level)+"\n");
-       s+="]";
+       var spacing=this.repeatStr(Logger.SpacingPerLevel,level);
+       var s;
+       if (level<max_level){
+           s="[\n";
+           for(var i=0;i<obj.length;i++)
+               s+=(spacing+i+":"+Logger.inspect(obj[i],Logger.excludeFunction,level+1,max_level)+"\n");
+           s+= this.repeatStr(Logger.SpacingPerLevel,level-1) + "]";
+       } else {
+           s ="["+obj.length +"]";
+       }
        return this.inspectCaption(obj)+s;
 	},
 	
+	SpacingPerLevel: "  ",
+	
 	inspect_hash: function(obj,predicate,level,max_level){
+       var spacing=this.repeatStr(Logger.SpacingPerLevel,level);
        var s="";
-       var attrs = this.sortedAttributes(obj,predicate);
-       attrs.each(function(attr){
-           if (!s.endWith("\n"))
-               s+="\n";
-	           var value = Logger.inspect(obj[attr],Logger.excludeFunction,level+1,max_level);
-  	           s+=(spacing+attr+":"+value+"\n");
-       });
+       if (level<max_level){
+           var attrs = this.sortedAttributes(obj,predicate);
+           attrs.each(function(attr){
+    	       var value;
+    	       try{
+    	           value = Logger.inspect(obj[attr],Logger.excludeFunction,level+1,max_level);
+    	       }catch(ex){
+    	           value="----- failed to get ---- " + (ex.msg||ex.message||ex);
+    	       }
+      	       s+=(spacing+attr+": "+value+"\n");
+           });
+       }
        return this.inspectCaption(obj) +"\n"+ s;
+	},
+	
+	inspect_function: function(obj,predicate,level,max_level){
+	   var str = (""+obj);
+	   if (str.indexOf("native")>-1) return "function [native method]";
+	   return "";
 	},
 	
 	inspect: function(obj,predicate,level,max_level){
@@ -96,17 +125,20 @@ Object.extend(Logger, {
 	   max_level=(max_level==undefined||max_level==null)?Logger.DefaultMaxLevel:max_level;
 	   if (obj==undefined)return "undefined";
 	   if (obj==null)return "null";
-       var spacing=this.repeatStr("  ",level);
+	   var objClass;
+	   try{objClass = obj.constructor}catch(ex){objClass = "unknown class"};
 	   if (level>=max_level){
-	       return (this.likeArray(obj)) ? this.inspect_array(obj,predicate,level,max_level) : this.inspectCaption(obj);
-	   } else if (level>0 && Logger.uninspectClasses.indexOf(obj.constructor)>-1){
+	       return (objClass==Function)?this.inspect_function(obj,predicate,level,max_level):
+	           (this.likeArray(obj)) ? this.inspect_array(obj,predicate,level,max_level): 
+	           this.inspectCaption(obj);
+	   } else if (level>0 && Logger.uninspectClasses.indexOf(objClass)>-1){
 	       return this.inspectCaption(obj);
-	   } else if (obj.constructor==Function){
-	       return "[function]";
+	   } else if (objClass==Function){
+	       return this.inspect_function(obj,predicate,level,max_level);
 	   } else if (this.likeArray(obj)) {
 	       return this.inspect_array(obj,predicate,level,max_level);
 	   } else {
-	       return this.inspect_hash(obj,predicate,level+1,max_level);
+	       return this.inspect_hash(obj,predicate,level,max_level);
 	   }
 	},
 	isActiveOnLoad: function(){
@@ -116,8 +148,10 @@ Object.extend(Logger, {
 	   document.cookie = ("loggerActive="+(value?"true":"false"))+";Fri, 31-Dec-2030 23:59:59;";
 	}
 });
+Logger.DefaultLogInterval = 500; //msec
 Logger.prototype = {
     initialize: function(element,logs){
+        this.logInterval = Logger.DefaultLogInterval;
         this.logs=logs||"";
         this.setElement($(element));
     },
@@ -128,12 +162,24 @@ Logger.prototype = {
         }else if (this.appendingType=="value"){
             this.element.value=this.element.value+"\n"+logs;
         }else if (this.appendingType=="innerHTML"){
-            this.element.innerHTML=this.element.innerHTML+"<br/>"+logs.escapeHTML(); //.gsub(/\n/,"<br/>");
+            var s = logs.gsub(/\n/,"___LF___").escapeHTML().gsub(/___LF___/,"<br/>").gsub(/ /,"&nbsp;");
+            this.element.innerHTML += "<div>"+ s + "</div>";
         }else{
             this.logs=this.logs||"";
             this.logs=this.logs+"\n"+logs;
         }
+        try{
+            this.scrollElement();
+        }catch(ex){
+        }
     },
+    scrollElement: function() {
+        if (!this.element)return;
+        this.window.document.body.scrollTop =
+        	this.window.document.body.scrollHeight -
+        	this.window.document.body.clientHeight;
+    },
+    
     setElement: function(element){
         var logs = (!this.appendingType)?this.logs:
             (this.appendingType=="value")?this.element.value:
@@ -155,22 +201,34 @@ Logger.prototype = {
         this.window = window.open('',windowName,"resizable=yes,scrollbars=yes,top=0,left=0");
         if (focus)
             this.window.focus();
-        this.window.document.write("<html><head><title>Log Window</title></head><body></body></html>");
-        //this.window.document.close();
+        if (!this.window.document.body) {
+            this.window.document.write("<html><head><title>Log Window</title></head><body></body></html>");
+            this.window.document.close();
+        }
         this.setElement(this.window.document.body||this.window.document.documentElement);
     },
     toLogStr: function(level,args){
         var s="";
-        args=$A(args);
-        var max_level = ((args.last())&&(args.last().constructor==Number))?args.pop():Logger.DefaultMaxLevel;
+        var max_level = ((args.length>2)&&(args.last())&&(args.last().constructor==Number))?args.pop():Logger.DefaultMaxLevel;
+        var predicateName = ((args.length>2)&&(args.last())&&(args.last().constructor==String))?args.pop().toLowerCase():null;
+        var predicate = (!predicateName)?null:
+            (predicateName=="properties")?Logger.excludeFunction:
+            (predicateName=="methods")?Logger.includeFunction:
+            (predicateName=="full")?Logger.includeAll:null;
         if (args.length>0) 
             s = this.formatMsg(level,args[0]);
         for(var i=1;i<args.length;i++)
-            s += ("\n inspect["+i+"] \n"+ Logger.inspect(args[i],null,null,max_level));
+            s += ("\n inspect["+i+"] \n"+ Logger.inspect(args[i],predicate,null,max_level));
         return s;
     },
     log: function(level,args){
-        this.append(this.toLogStr(level,args));
+        this.waitingLogCount = this.waitingLogCount || 0;
+        this.waitingLogCount++; 
+        args=$A(args);
+        setTimeout(function(){
+            this.append(this.toLogStr(level,args));
+            this.waitingLogCount--;
+        }.bind(this), this.logInterval * this.waitingLogCount);
     },
     debug: function(){this.log("debug",arguments);},
     info: function(){this.log("info",arguments);},
@@ -183,6 +241,7 @@ if (window["top"] && top.tracer) {
     
     Object.extend(logger, {
         log: function(level,args){
+            args=$A(args);
             var s = this.toLogStr(level,args).gsub(/\n/,"___LF___").escapeHTML().gsub(/___LF___/,"<br/>").gsub(/ /,"&nbsp;");
             
             switch(level){
