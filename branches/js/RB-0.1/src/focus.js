813cbaff4b224c5472b5c44807e4e5b4762609eb
akm2000 <akm2000@907533e0-d619-0410-9558-a12620d830f9>
2006-09-28 03:41:53 +0000

    HTMLElement.Focus.Highlightを現段階ではIEのみ有効とすることにした。

--- branches/js/RB-0.1/src/focus.js
+++ branches/js/RB-0.1/src/focus.js
@@ -5,6 +5,9 @@
  *     prototype.js 
  *     prototype_ext.js 
  *
+ * optional
+ *     effects.js
+ *
  * @author T.Akima
  * @copyright T.Akima
  * @license LGPL
@@ -28,9 +31,26 @@ Object.extend(HTMLElement.Focus.Highlight.DefaultOptions,
         {   focusEventName: "focus",
             blurEventName: "blur" }
 );
+Object.extend(HTMLElement.Focus.Highlight.DefaultOptions, 
+    (window["Effect"] && Effect.Highlight) ?
+        {   focusEffect: function(event){
+                new Effect.Highlight(Event.element(event));
+            },
+            blurEffect: null } :
+            
+        {   focusEffect: function(event){
+                var element = Event.element(event);
+                element.style.backgroundColor = "#ffcccc";
+            },
+            blurEffect: function(event){
+                var element = Event.element(event);
+                element.style.backgroundColor = "";
+            } }
+);
+
 Event.observe(window, "load", function(event){
     HTMLElement.Focus.Highlight.DefaultOptions.element =
-        (/MSIE/.test(navigator.appVersion)) ? document.body : document; 
+        (/MSIE/.test(navigator.appVersion)) ? document.body : document.documentElement; 
     var waitingList = HTMLElement.Focus.Highlight.waitingList;
     HTMLElement.Focus.Highlight.waitingList = null;
     for(var i = 0; i < waitingList.length; i++) {
@@ -49,26 +69,60 @@ HTMLElement.Focus.Highlight.prototype = {
             this.activate();
     },
     activate: function(){
+        
+        //2006/09/28現在、IE以外で一括でフォーカスをゲットする方法が分からないっす。
+        if (!/MSIE/.test(navigator.appVersion)){
+            if (window["logger"])
+                logger.warn("IE以外のブラウザでは、HTMLElement.Focus.Highlightは無効です");
+            return;
+        }
         if (HTMLElement.Focus.Highlight.waitingList){
             HTMLElement.Focus.Highlight.waitingList.push(this);
             return;
         }
         if (!this.element)
             throw new Error("element not specified.");
-        logger.debug("activate: this.options.focusEventName=" + this.options.focusEventName);
-        Event.observe(this.element, this.options.focusEventName, this.focused.bindAsEventListener(this), false);
-        //Event.observe(this.element, this.options.blurEventName, this.blured.bindAsEventListener(this), false);
+        if (this.options.focusEffect){
+            if (!this.focusedHandler)
+                this.focusedHandler = this.focused.bindAsEventListener(this);
+            Event.observe(this.element, this.options.focusEventName,
+                this.focusedHandler, false);
+        }
+        if (this.options.blurEffect){
+            if (!this.blurHandler)
+                this.blurHandler = this.blured.bindAsEventListener(this); 
+            Event.observe(this.element, this.options.blurEventName, 
+                this.blurHandler, false);
+        }
+    },
+    deactivate: function(){
+        if (HTMLElement.Focus.Highlight.waitingList){
+            HTMLElement.Focus.Highlight.waitingList.remove(this);
+            return;
+        }
+        if (this.focusedHandler){
+            Event.stopObserving(this.element, this.options.focusEventName,
+                this.focusedHandler, false);
+            this.focusedHandler = null;
+        }
+        if (this.blurHandler){
+            Event.stopObserving(this.element, this.options.blurEventName, 
+                this.blurHandler, false);
+            this.blurHandler = null;
+        }
     },
     focused: function(event){
         var element = Event.element(event);
         if (element==window || element==document || 
             element==document.body || element==document.documentElement)
             return;
-        
-        //logger.debug("focused", element);
-        
-        new Effect.Highlight(element, {startcolor: '#ff7777'});
+        this.options.focusEffect(event);
     },
     blured: function(event){
+        var element = Event.element(event);
+        if (element==window || element==document || 
+            element==document.body || element==document.documentElement)
+            return;
+        this.options.blurEffect(event);
     }
 }
