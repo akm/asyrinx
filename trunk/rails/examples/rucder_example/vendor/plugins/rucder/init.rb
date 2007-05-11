e90f5c3227ea848b0464f47f9100341bcd7cd749
akm2000 <akm2000@907533e0-d619-0410-9558-a12620d830f9>
2007-05-11 02:38:25 +0000

    DBへのコネクションをフックすることができました。

--- trunk/rails/examples/rucder_example/vendor/plugins/rucder/init.rb
+++ trunk/rails/examples/rucder_example/vendor/plugins/rucder/init.rb
@@ -1,6 +1,11 @@
 # this will be invoked in Rails::Initializer.run after eval the block
-
-require 'rucder'
-ActiveRecord::Base.class_eval do
-  include ActiveRecord::Rucder
+if ENV["RAILS_ENV"] == "development"
+  require 'rucder_service'
+  require 'rucder_database_statements'
+  
+  conn = ActiveRecord::Base.connection
+  conn.class.class_eval do
+    include ActiveRecord::Rucder::DatabaseStatements
+  end
+  conn.rucder_enabled = true
 end
---
+++ trunk/rails/examples/rucder_example/vendor/plugins/rucder/lib/rucder_service.rb
@@ -0,0 +1,35 @@
+# Rucder
+module ActiveRecord
+  module Rucder
+    def self.rucder_service
+      @rucder_service
+    end
+    def self.rucder_service=(new_service)
+      @rucder_service = new_service
+    end
+
+    def self.notify_evnet(event, context)
+      ActiveRecord::Rucder.rucder_service ||= LogService.new
+      ActiveRecord::Rucder.rucder_service.call(event, context)
+    end
+    
+    module Observable
+      def notify_evnet(event, arguments)
+        ActiveRecord::Rucder.notify_evnet(event, {:arguments => arguments}) if rucder_enabled && !@in_notification
+        @in_notification = true
+        begin
+          return yield if defined? yield
+        ensure
+          @in_notification = false
+        end
+      end
+    end
+    
+    class LogService
+      def call(event, context)
+        # ActiveRecord::Base.logger.debug("#{event.to_s} #{context.inspect}\n  " << caller(trace_level).join("\n  "))
+        ActiveRecord::Base.logger.debug("#{event.to_s} #{context.inspect}")
+      end
+    end
+  end
+end
