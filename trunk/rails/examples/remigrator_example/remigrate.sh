9094c44ca9e7764281f52f747047370594d2f42b
okamoto443 <okamoto443@907533e0-d619-0410-9558-a12620d830f9>
2007-05-30 07:36:30 +0000

    RAILS_ENV=testを指定するように修正しました

--- trunk/rails/examples/remigrator_example/remigrate.sh
+++ trunk/rails/examples/remigrator_example/remigrate.sh
@@ -3,9 +3,9 @@
 RAILS_ROOT=/home/rails/okamoto_test/remigrator_example
 cd $RAILS_ROOT
 echo "migrate down... "
-eval rake db:migrate VERSION=0
+eval rake db:migrate VERSION=0 RAILS_ENV=test
 echo "-----END-------"
 echo "migrate up... "
-eval rake db:migrate
+eval rake db:migrate RAILS_ENV=test
 echo "-----END-------"
 exit
