2cbbc6eb94f88f2d04b6eecdc87334db74dbf2d9
okamoto443 <okamoto443@907533e0-d619-0410-9558-a12620d830f9>
2007-06-07 09:28:36 +0000

    remigrateのtaskを作成しました。

--- rails/examples/remigrator_example/vendor/plugins/remigrater/tasks/remigrater_tasks.rake
+++ rails/examples/remigrator_example/vendor/plugins/remigrater/tasks/remigrater_tasks.rake
@@ -1,4 +1,11 @@
-# desc "Explaining what the task does"
-# task :remigrater do
-#   # Task goes here
-# end
+require 'active_record'
+desc 'Migrate database to a version, then back up to current'
+task :remigrate  => :environment do
+  ENV['VERSION'] ||= '0'
+  print "Migrating to version #{ENV['VERSION']}..."
+  ActiveRecord::Migrator.migrate("db/migrate/", ENV["VERSION"] ? ENV["VERSION"].to_i : nil)
+  puts ' done'
+  print "Migrating to current_version ..."
+  ActiveRecord::Migrator.migrate("db/migrate/", nil)
+  puts ' done'
+end
--- rails/examples/remigrator_example/vendor/plugins/remigrater/test/remigrater_test.rb
+++ rails/examples/remigrator_example/vendor/plugins/remigrater/test/remigrater_test.rb
@@ -1,8 +1,22 @@
+require(File.join(File.dirname(__FILE__), '../../../../','config', 'boot'))
+
 require 'test/unit'
 
+require 'rake'
+require 'rake/testtask'
+require 'rake/rdoctask'
+
+require 'tasks/rails'
+
 class RemigraterTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+  def test_remigrate_task
+    # from versiont=3 to version=5
+    ENV['VERSION']="3"
+    Rake::Task["db:migrate"].invoke
+    before_ver = ActiveRecord::Migrator.current_version.to_i
+    Rake::Task[:remigrate].invoke
+    after_ver = ActiveRecord::Migrator.current_version.to_i
+    assert_equal 3, before_ver
+    assert_equal 5, after_ver
   end
 end
